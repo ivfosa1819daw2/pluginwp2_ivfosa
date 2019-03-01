6272da48e1de73921630d8704c7f44be4dafd457
ivfosa1819daw2 <ivfosa1819daw2@gmail.com>
2019-03-01 19:03:01 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: https://github.com/ivfosa1819daw2/pluginwp2_ivfosa
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Ivan Fontclara
+Author URI: https://github.com/ivfosa1819daw2/
+License: GNU GPLv3 
+*/ 
+?>
