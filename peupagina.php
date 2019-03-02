85883923f48750b83447ed1000fe0b63cc7e7290
ivfosa1819daw2 <ivfosa1819daw2@gmail.com>
2019-03-02 19:02:22 +0100

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -18,8 +18,16 @@ $nom_funcio="nouPeuPagDef";
 add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
 }
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
 
 function nouPeuPagDef(){
 include_once('formPeuPag.php');
 }
+function afegPeuPagDef(){
+    echo "<div style='color: red;
+    font-size: 10px;
+    margin: 10px;'>".get_option('footer_text')."</div>";
+}
+
+
 ?>
